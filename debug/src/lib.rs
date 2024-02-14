3be7a646e9ddcec7eb927a652cd8427f293e9e20
Jhonas <jhonaspradomoura@gmail.com>
2024-02-14 14:21:47 -0300

    fix: use syn::Type instead of manually checking segments

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,44 +1,60 @@
-use proc_macro::TokenStream;
 use quote::ToTokens;
 use syn::spanned::Spanned;
 
-struct AssociativeType {
-    trait_ident: syn::Ident,
-    colon2: syn::token::PathSep,
-    item_ident: syn::Ident,
-}
+// struct AssociativeType {
+//     trait_ident: syn::Ident,
+//     colon2: syn::token::PathSep,
+//     item_ident: syn::Ident,
+// }
 
-impl syn::parse::Parse for AssociativeType {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let trait_ident = input.parse()?;
-        let colon2 = input.parse()?;
-        let item_ident = input.parse()?;
-        Ok(Self {
-            trait_ident,
-            colon2,
-            item_ident,
-        })
-    }
-}
+// impl syn::parse::Parse for AssociativeType {
+//     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+//         let trait_ident = input.parse()?;
+//         let colon2 = input.parse()?;
+//         let item_ident = input.parse()?;
+//         Ok(Self {
+//             trait_ident,
+//             colon2,
+//             item_ident,
+//         })
+//     }
+// }
 
-struct BoundAssociativeType {
-    associative_ty: AssociativeType,
-    colon: syn::Token![:],
-    binding: syn::Path,
-}
+// // impl std::convert::Into<syn::Type> for AssociativeType {
+// //     fn into(self) -> syn::Type {
+// //         syn::Type::Path(syn::TypePath{
+// //             qself: None,
+// //             path: syn::Path::
+// //         })
+// //     }
+// // }
 
-impl syn::parse::Parse for BoundAssociativeType {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let associative_ty = input.parse()?;
-        let colon = input.parse()?;
-        let binding = input.parse()?;
-        Ok(Self {
-            associative_ty,
-            colon,
-            binding,
-        })
-    }
-}
+// impl quote::ToTokens for AssociativeType {
+//     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+//         self.trait_ident.to_tokens(tokens);
+//         self.colon2.to_tokens(tokens);
+//         self.item_ident.to_tokens(tokens);
+//     }
+// }
+
+// struct BoundAssociativeType {
+//     associative_ty: AssociativeType,
+//     colon: syn::Token![:],
+//     binding: syn::Path,
+// }
+
+// impl syn::parse::Parse for BoundAssociativeType {
+//     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+//         let associative_ty = input.parse()?;
+//         let colon = input.parse()?;
+//         let binding = input.parse()?;
+//         Ok(Self {
+//             associative_ty,
+//             colon,
+//             binding,
+//         })
+//     }
+// }
 
 struct ImplData<'a> {
     generics: &'a syn::Generics,
@@ -115,20 +131,20 @@ struct FieldsData {
     ty: ::syn::Type,
 }
 
-enum GenericVariant<'a> {
-    Associative(&'a syn::punctuated::Punctuated<syn::PathSegment, syn::Token![::]>),
+enum GenericVariant {
+    Associative(syn::Type),
     SameGeneric,
     Phantom,
     DifferentFromGeneric,
 }
 
-impl<'a> GenericVariant<'a> {
+impl GenericVariant {
     pub fn prioritize(new_variant: Self, old_variant: Self) -> Self {
         match (&new_variant, &old_variant) {
             (_, Self::Phantom) => Self::Phantom,
             (Self::Phantom, _) => Self::Phantom,
-            (_, Self::Associative(a)) => Self::Associative(a),
-            (Self::Associative(a), _) => Self::Associative(a),
+            (_, Self::Associative(_)) => old_variant,
+            (Self::Associative(_), _) => new_variant,
             (_, Self::SameGeneric) => Self::SameGeneric,
             (Self::SameGeneric, _) => Self::SameGeneric,
             _ => new_variant,
@@ -146,7 +162,7 @@ impl<'a> GenericVariant<'a> {
 }
 
 #[proc_macro_derive(CustomDebug, attributes(debug, bound))]
-pub fn derive(input: proc_macro::TokenStream) -> TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
     let struct_name = ast.ident;
     let fields_info = match &ast.data {
@@ -156,7 +172,7 @@ pub fn derive(input: proc_macro::TokenStream) -> TokenStream {
         },
         _ => unimplemented!(),
     };
-    process_outer_attrs(ast.attrs);
+    // process_outer_attrs(ast.attrs);
     let (fields, _possible_errors): (
         ::std::vec::Vec<FieldsData>,
         ::std::vec::Vec<::std::option::Option<::syn::Error>>,
@@ -178,27 +194,27 @@ pub fn derive(input: proc_macro::TokenStream) -> TokenStream {
     proc_macro::TokenStream::from(expanded)
 }
 
-fn process_outer_attrs(
-    attrs: ::std::vec::Vec<syn::Attribute>,
-) -> ::std::result::Result<(), syn::Error> {
-    for attr in attrs {
-        println!("{}", attr.to_token_stream());
-        if attr.path().is_ident("debug") {
-            let mut bound_ty = String::new();
-            let mut binding = String::new();
-            attr.parse_nested_meta(|meta| {
-                if meta.path.is_ident("bound") {
-                    let value = meta.value()?;
-                    println!("TYPE VALUE {}", value);
-                    Ok(())
-                } else {
-                    Err(syn::Error::new(meta.path.span(), "unsupported attribute"))
-                }
-            })?
-        }
-    }
-    unimplemented!()
-}
+// fn process_outer_attrs(
+//     attrs: ::std::vec::Vec<syn::Attribute>,
+// ) -> ::std::result::Result<(), syn::Error> {
+//     for attr in attrs {
+//         println!("{}", attr.to_token_stream());
+//         if attr.path().is_ident("debug") {
+//             let mut bound_ty = String::new();
+//             let mut binding = String::new();
+//             attr.parse_nested_meta(|meta| {
+//                 if meta.path.is_ident("bound") {
+//                     let value = meta.value()?;
+//                     println!("TYPE VALUE {}", value);
+//                     Ok(())
+//                 } else {
+//                     Err(syn::Error::new(meta.path.span(), "unsupported attribute"))
+//                 }
+//             })?
+//         }
+//     }
+//     unimplemented!()
+// }
 
 fn process_fields(
     ast_fields: &syn::punctuated::Punctuated<syn::Field, syn::token::Comma>,
@@ -250,9 +266,9 @@ fn debug_attrs(
     Ok(None)
 }
 
-fn compute_trait_bounds<'a>(
+fn compute_trait_bounds(
     mut generics: syn::Generics,
-    types: &'a ::std::vec::Vec<syn::Type>,
+    types: &::std::vec::Vec<syn::Type>,
 ) -> ::std::result::Result<syn::Generics, syn::Error> {
     let mut where_generics = generics.clone();
     for param in &mut generics.params {
@@ -266,15 +282,25 @@ fn compute_trait_bounds<'a>(
                 }) {
                     type_param.bounds.push(syn::parse_quote!(::std::fmt::Debug));
                 }
-                if let Some(GenericVariant::Associative(at)) =
+                if let Some(GenericVariant::Associative(parsed_ty)) =
                     generic_variant.iter().find(|v| match v {
                         GenericVariant::Associative(_) => true,
                         _ => false,
                     })
                 {
-                    let parsed_ty: syn::Type = syn::parse2(at.to_token_stream())?;
                     let where_clause = where_generics.make_where_clause();
-                    if where_clause.predicates.is_empty() {
+                    if !where_clause.predicates.iter().any(|pred| {
+                        if let syn::WherePredicate::Type(syn::PredicateType {
+                            bounded_ty,
+                            ..
+                        }) = pred
+                        {
+                            quote::quote!(#bounded_ty).to_string()
+                                == quote::quote!(#parsed_ty).to_string()
+                        } else {
+                            false
+                        }
+                    }) {
                         let bound = syn::TypeParamBound::Trait(syn::TraitBound {
                             paren_token: None,
                             modifier: syn::TraitBoundModifier::None,
@@ -320,10 +346,10 @@ fn compute_trait_bounds<'a>(
     Ok(generics)
 }
 
-fn compute_generic_variant<'a>(
-    types: &'a ::std::vec::Vec<syn::Type>,
-    generic_ident: &'a syn::Ident,
-) -> ::std::vec::Vec<GenericVariant<'a>> {
+fn compute_generic_variant(
+    types: &::std::vec::Vec<syn::Type>,
+    generic_ident: &syn::Ident,
+) -> ::std::vec::Vec<GenericVariant> {
     types
         .iter()
         .map(|ty| match ty {
@@ -334,17 +360,10 @@ fn compute_generic_variant<'a>(
                 GenericVariant::DifferentFromGeneric,
                 |variant, segment| {
                     // Associative type from this generic
-                    let inner_variant = if segments.len() == 3
-                        && segments.clone().into_pairs().fold(false, |res, pair| {
-                            let match_structure = match pair {
-                                syn::punctuated::Pair::Punctuated(seg, _) => {
-                                    &seg.ident == generic_ident
-                                },
-                                syn::punctuated::Pair::End(_) => true,
-                            };
-                            res && match_structure
-                        }) {
-                        GenericVariant::Associative(segments)
+                    let inner_variant = if let Ok(associative_type) =
+                        syn::parse2(segments.to_token_stream())
+                    {
+                        GenericVariant::Associative(associative_type)
                     } else if &segment.ident == generic_ident {
                         GenericVariant::SameGeneric
                     } else {
@@ -361,10 +380,10 @@ fn compute_generic_variant<'a>(
         .collect()
 }
 
-fn recursive_search_over_arguments_for_type<'a>(
-    args: &'a syn::PathArguments,
-    generic_ident: &'a syn::Ident,
-) -> GenericVariant<'a> {
+fn recursive_search_over_arguments_for_type(
+    args: &syn::PathArguments,
+    generic_ident: &syn::Ident,
+) -> GenericVariant {
     if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
         args: inner_args,
         ..
@@ -378,17 +397,10 @@ fn recursive_search_over_arguments_for_type<'a>(
                     ..
                 })) => {
                     // Associative type from this generic
-                    let seg_variant = if segments.len() == 2
-                        && segments.clone().into_pairs().fold(true, |res, pair| {
-                            let match_associative = match pair {
-                                syn::punctuated::Pair::Punctuated(seg, _) => {
-                                    &seg.ident == generic_ident
-                                },
-                                syn::punctuated::Pair::End(_) => true,
-                            };
-                            res && match_associative
-                        }) {
-                        GenericVariant::Associative(segments)
+                    let seg_variant = if let Ok(associative_type) =
+                        syn::parse2(segments.to_token_stream())
+                    {
+                        GenericVariant::Associative(associative_type)
                     } else {
                         segments.iter().fold(
                             GenericVariant::DifferentFromGeneric,
